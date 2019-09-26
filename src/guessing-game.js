e5704128ca5135c44ff84894bc3c620e60578cb4
Natallia <55149770+natallia-js@users.noreply.github.com>
2019-09-26 16:44:42 +0300

    feat: implement a GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,35 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.res = undefined;
+    }
 
     setRange(min, max) {
+        if (min > max) {
+            throw new Error("Wrong range!");
+        }
 
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
+        this.res = Math.round((this.max + this.min) / 2);
 
+        return this.res;
     }
 
     lower() {
-
+        if (this.min < this.max) {
+            this.max = this.res;
+        }
     }
 
     greater() {
-
+        if (this.min < this.max) {
+            this.min = this.res;
+        }
     }
 }
 
